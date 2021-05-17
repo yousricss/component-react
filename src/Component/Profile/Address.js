a78fd329065fb8a27ed844175afd9d001c08c7be
yousricss <yousri_bahloul@yahoo.fr>
2021-05-17 10:17:35 +0200

    first-commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+            <p>Route de mahdia KM8 Sfax</p>
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-with-glasses.jpg" alt=""/>
+
+        </div>
+    )
+}
+
+export default ProfilePhoto
